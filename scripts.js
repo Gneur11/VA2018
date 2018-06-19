74d78dec125e89c57f168996768c311d662e95a9
Filippo Menis <filippomenis@yahoo.it>
2018-06-19 18:35:48 +0200

    added new graph

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ function bar(name){
 					.entries(data)
 	                .sort(function(a, b){ return d3.descending(a.values, b.values);});
 
-		console.log(ordered);
+	//	console.log(ordered);
 
 								
 	//	var margin = {top: 50, right: 20, bottom: 200, left: 50};
@@ -27,10 +27,10 @@ function bar(name){
 			return d3.ascending(x.index, y.index);
 			})
 			
-			var x = d3.scaleLinear().domain([0, d3.max(count)]).range([0, w-50]),
+			var x = d3.scaleLinear().domain([0, d3.max(count)]).range([0, w-20]),
 			y = d3.scaleLinear().domain([0, 7]).range([0,h]);
 			
-			console.log(d3.max(count));
+		//	console.log(d3.max(count));
 			ordered = ordered.sort();
 			var xAxis = d3.axisBottom(x);
 			var yAxis = d3.axisLeft(y);
@@ -145,10 +145,10 @@ function scatter(name){
 			var ordered = d3.nest()
 					.key(function(d){return d['car-id'];})
 					.entries(data);
-			console.log(ordered);
+		//	console.log(ordered);
 			
 			var max = d3.max(ordered,function(d){return d.values.length;});
-			console.log(max);
+		//	console.log(max);
 			
 			//var margin = {top: 20, right: 20, bottom: 50, left: 50};
 			var h = document.getElementById(name).scrollHeight;
@@ -157,7 +157,7 @@ function scatter(name){
 				h = h - margin.top - margin.bottom;
 
 			
-			var x = d3.scaleLinear().domain([0, ordered.length]).range([0, w]),
+			var x = d3.scaleLinear().domain([0, ordered.length]).range([0, w-margin.right]),
 			y = d3.scaleLinear().domain([0, max]).range([h,0]);
 
 			var xAxis = d3.axisBottom(x);
@@ -207,3 +207,105 @@ function scatter(name){
 			
 			});
 	};
+
+function time(name){
+	d3.csv("Lekagul Sensor Data.csv").then(function(data){
+			var base = data;
+		//	console.log(base);
+			var ordered = d3.nest()
+					.key(function(d){return d['Timestamp'];})
+					.entries(data);
+		///	console.log(ordered);
+			
+			var date = "2015-05-01 08:32:09"
+			var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
+			var c = parseTime(date);
+			
+			var format = d3.timeFormat("%y-%m-%d");
+		//	console.log(c);
+		//	console.log(format(c));
+		//	console.log(base[0].Timestamp);
+			
+			ordered.forEach(function(d,i) {   
+				var time = parseTime(d.key);
+				d.key = format(time);
+				})
+			console.log(ordered);
+			
+			var ordered1 = d3.nest()
+						.key(function(d){return d['key'];})
+						.entries(ordered);
+			console.log(ordered1);
+			
+			var h = document.getElementById(name).scrollHeight;
+			var w = document.getElementById(name).offsetWidth;
+			    w = w - margin.left - margin.right;
+				h = h - margin.top - margin.bottom;
+
+			var max = d3.max(ordered1,function(d){return d.values.length;});
+			console.log(max);
+			
+			var x = d3.scaleLinear().range([0, w-margin.right]),
+			y = d3.scaleLinear().range([h,0]);
+			
+			
+			console.log(d3.extent(ordered1, function(d) {return d.key}));
+			console.log(d3.extent(ordered1, function(d) {return d.values.length}));
+			
+			x.domain([0,ordered1.length]);
+			y.domain(d3.extent(ordered1, function(d) {return d.values.length;}));
+			
+			
+			var xAxis = d3.axisBottom(x);
+			var yAxis = d3.axisLeft(y);
+			
+			console.log(x(5));
+			
+			var line = d3.line()
+						.x(function(d,i) {return x(i);})
+						.y(function(d) {return y(d.values.length);})
+						;//.curve(d3.curveCardinal);
+
+			
+			var svg = d3.select("#"+name).append("svg")
+					.attr("width", w + margin.left + margin.right)
+					.attr("height", h + margin.top + margin.bottom)
+					.append("g")
+					.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+					
+			svg.append("g")
+				  .attr("transform", "translate(0," + h + ")")
+				  .call(d3.axisBottom(x));
+
+			  // text label for the x axis
+			svg.append("text")             
+				  .attr("transform",
+						"translate(" + (w/2) + " ," + 
+									   (h + margin.top + 20) + ")")
+				  .style("text-anchor", "middle")
+				  .text("Day");
+			
+			 // Add the y Axis
+			svg.append("g")
+				.call(d3.axisLeft(y));
+	
+			// text label for the y axis
+			svg.append("text")
+				.attr("transform", "rotate(-90)")
+				  .attr("y", 0 - margin.left)
+				  .attr("x",0 - (h / 2))
+				  .attr("dy", "1em")
+				  .style("text-anchor", "middle")
+				  .text("Traffic (# of readings)");
+			
+			svg.append("path")
+				.datum(ordered1)
+				.attr("fill","none")
+				.attr("stroke", "steelblue")
+				.attr("class","line")
+				.attr("stroke-width",1.5)
+				.attr("d",line);
+	});
+	
+};
+	
