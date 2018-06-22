9b5d38dde38a9690b7af1f85c691e65d37ac4fc3
Filippo Menis <filippomenis@yahoo.it>
2018-06-22 17:36:24 +0200

    added filtering to top-left quadrant (no monthly) and layout improvements

--- scripts.js
+++ scripts.js
@@ -1,49 +1,107 @@
-var margin = {top: 50, right: 10, bottom: 150, left: 50};
+var margin = {top: 40, right: 10, bottom: 150, left: 50};
 
 var currentGraphs = {"a": "bar", "b": "scatter", "c":"time","d":"none",
-					"inputA":"a","inputB":"b","inputC":"c","inputD":"none"};
-	
-	
-	
-	
-function bar(name){
-		currentGraphs.a = "bar";
-		currentGraphs.inputA = name;
-		d3.csv("Lekagul Sensor Data.csv").then(function(data){
-			var base = data;
-		//	console.log(base);
+					"inputA":"a","inputB":"b","inputC":"c","inputD":"none",
+					"filterA": "none"};
+					
+function initChangeA(){
+		var s = document.getElementById("filterA");
+		currentGraphs.filterA = "" + s[s.selectedIndex].value;
+		console.log(currentGraphs.filterA);
+		bar();
+		}
+
+function back(){
+	x = document.getElementById("backA");
+	x.style.display = "none";
+	bar();
+}
+		
+function bar(){
+		var name = "a";
+		var selection = document.getElementById("filterA");
+		var filter;
+		if(currentGraphs.filterA == "none"){
+			filter = "total";
+		} else {
+			d3.select("#svg"+name).remove()
+			filter = currentGraphs.filterA;
+		}
+		console.log(filter);
+		if(filter ==  "total"){
+			d3.csv("Lekagul Sensor Data.csv").then(function(data){
 			var ordered = d3.nest()
 					.key(function(d){return d['car-type'];})
 					.entries(data)
 	                .sort(function(a, b){ return d3.descending(a.values, b.values);});
 		console.log(ordered);
+		currentGraphs.a = "bar";
+		currentGraphs.inputA = name;
+		currentGraphs.filterA = filter;
+		auxBar(ordered,name);
+		});
+		} else if (filter == "2016"  || filter == "2015"){
+			d3.csv("Lekagul Sensor Data.csv").then(function(data){
+				if(filter == "2016"){
+						currentGraphs.a = "bar";
+						currentGraphs.inputA = name;
+						currentGraphs.filterA = filter;
+						var base = data;
+						var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
+						var format = d3.timeFormat("%y");
+						base.forEach(function(d,i) {   
+							var time = parseTime(d.Timestamp);
+							d.Timestamp = format(time);
+							})						
+						base = base.filter(function(d) {return d['Timestamp'] == "16";})
+						var ordered = d3.nest()
+								.key(function(d){return d['car-type'];})
+								.entries(base)
+								.sort(function(a, b){ return d3.descending(a.values, b.values);});
+							auxBar(ordered,name);
+						//AGGIUGNI I CURRENTGRAPHS
+				} else if (filter == "2015"){
+						currentGraphs.a = "bar";
+						currentGraphs.inputA = name;
+						currentGraphs.filterA = filter;
+						var base = data;
+						var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
+						var format = d3.timeFormat("%y");
+						base.forEach(function(d,i) {   
+							var time = parseTime(d.Timestamp);
+							d.Timestamp = format(time);
+							})
+						base = base.filter(function(d) {return d['Timestamp'] == "15";})
+						var ordered = d3.nest()
+								.key(function(d){return d['car-type'];})
+								.entries(base)
+								.sort(function(a, b){ return d3.descending(a.values, b.values);});
+							auxBar(ordered,name);
+				}
+			})
+		};
+		
+		
+};
 
-								
-	//	var margin = {top: 50, right: 20, bottom: 200, left: 50};
-			var h = document.getElementById(name).scrollHeight;
+function auxBar(data,name){
+			var h = document.getElementById(name).clientHeight;
 			var w = document.getElementById(name).offsetWidth;
 			    w = w - margin.left - margin.right;
 				h = h - margin.top - margin.bottom;
-			
-			
 			var i;
 			count = [];
-			for (i=0; i< ordered.length; i++){
-				count[i] = ordered[i].values.length;
+			for (i=0; i< data.length; i++){
+				count[i] = data[i].values.length;
 			}
-			
 			count.sort(function(x, y){
 			return d3.ascending(x.index, y.index);
 			})
-			
 			var x = d3.scaleLinear().domain([0, d3.max(count)]).range([0, w-20]),
 			y = d3.scaleLinear().domain([0, 7]).range([0,h]);
-			
-		//	console.log(d3.max(count));
-			ordered = ordered.sort();
+			ordered = data.sort();
 			var xAxis = d3.axisBottom(x);
 			var yAxis = d3.axisLeft(y);
-
 			var canvas = d3.select("#"+name).append("svg")
 					.attr("width", w + margin.left + margin.right)
 					.attr("height", h + margin.top + margin.bottom)
@@ -59,7 +117,7 @@ function bar(name){
 			canvas.append("text")             
 				  .attr("transform",
 						"translate(" + (w/2) + " ," + 
-									   (h + margin.top + 20) + ")")
+									   (h + margin.top) + ")")
 				  .style("text-anchor", "middle")
 				  .text("Number of occurrences");
 			
@@ -75,9 +133,7 @@ function bar(name){
 					.attr("x", -25)
 					.attr("y", function(d,i){return y(i)+20;})
 					.text(function(d) {return d.key;});
-*/
-			
-			
+*/	
 			// text label for the y axis
 			canvas.append("text")
 				.attr("transform", "rotate(-90)")
@@ -92,8 +148,8 @@ function bar(name){
 				.style("opacity", 0);
 
 			
-		canvas.selectAll("rect")
-					.data(ordered)
+			canvas.selectAll("rect")
+					.data(data)
 					.enter()
 					.append("rect")
 					.attr("width",function (d){return x(d.values.length);})
@@ -117,48 +173,66 @@ function bar(name){
 							currentGraphs.a = "gateBar";
 							currentGraphs.inputA = d.key;
 							});
-		});
-};
+}
 
 function gateBar(key){
+	var b = document.getElementById("backA");
+	b.style.display = "block";
 	d3.csv("Lekagul Sensor Data.csv").then(function(data){
 			console.log(key);
-			var filtered = d3.nest()
+			var filter = currentGraphs.filterA;
+			var filtered; 
+			if(filter == "2015"){
+				var base = data;
+				var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
+				var format = d3.timeFormat("%y");
+				base.forEach(function(d,i) {   
+				var time = parseTime(d.Timestamp);
+				d.Timestamp = format(time);})						
+				base = base.filter(function(d) {return d['Timestamp'] == "15";})
+				filtered = d3.nest()
+					.key(function(d){return d['car-type'];})
+					.key(function(d){return d['gate-name'];})
+					.entries(base)
+					.filter(function(d) {return d.key == key;});
+			} else if (filter=="2016") {
+				var base = data;
+				var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
+				var format = d3.timeFormat("%y");
+				base.forEach(function(d,i) {   
+				var time = parseTime(d.Timestamp);
+				d.Timestamp = format(time);})						
+				base = base.filter(function(d) {return d['Timestamp'] == "16";})
+				filtered = d3.nest()
+					.key(function(d){return d['car-type'];})
+					.key(function(d){return d['gate-name'];})
+					.entries(base)
+					.filter(function(d) {return d.key == key;});
+			}
+			else if (filter=="total") {
+				filtered = d3.nest()
 					.key(function(d){return d['car-type'];})
 					.key(function(d){return d['gate-name'];})
 					.entries(data)
 					.filter(function(d) {return d.key == key;});
-					
-				
-				
-					console.log(filtered);
-	
-		var f = filtered[0].values;
-	    f.sort(function(a, b){ return d3.descending(a.values, b.values);});
-		
-		console.log(f);
-		
-		
-		
-		var h = document.getElementById("a").scrollHeight;
-		var w = document.getElementById("a").offsetWidth;
-			    w = w - margin.left - margin.right;
-				h = h - margin.top - margin.bottom;
-		
-		
-		var i;		
-		count = [];
-		for (i=0; i< filtered[0].values.length; i++){
-			count[i] = filtered[0].values[i].values.length;
-		}
-		
-		var x = d3.scaleLinear().domain([0, d3.max(count)]).range([0, w-20]),
-		y = d3.scaleLinear().domain([0, count.length]).range([0,h]);
-		var xAxis = d3.axisBottom(x);
-		var yAxis = d3.axisLeft(y);
-
+			}
+			var f = filtered[0].values;
+			f.sort(function(a, b){ return d3.descending(a.values, b.values);});		
+			var h = document.getElementById("a").clientHeight;
+			var w = document.getElementById("a").offsetWidth;
+					w = w - margin.left - margin.right;
+					h = h - margin.top - margin.bottom;
+			var i;		
+			console.log(f);
+			count = [];
+			for (i=0; i< filtered[0].values.length; i++){
+				count[i] = filtered[0].values[i].values.length;
+			}
+			var x = d3.scaleLinear().domain([0, d3.max(count)]).range([0, w-20]),
+			y = d3.scaleLinear().domain([0, count.length]).range([0,h]);
+			var xAxis = d3.axisBottom(x);
+			var yAxis = d3.axisLeft(y);
 			d3.select("#svga").remove();
-			
 			var canvas = d3.select("#a").append("svg")
 					.attr("width", w + margin.left + margin.right)
 					.attr("height", h + margin.top + margin.bottom)
@@ -201,8 +275,7 @@ function gateBar(key){
 				  .style("text-anchor", "middle")
 				  .text("Sensor");      
 			
-		
-		canvas.selectAll("rect")
+			canvas.selectAll("rect")
 					.data(f)
 					.enter()
 					.append("rect")
@@ -210,29 +283,21 @@ function gateBar(key){
 					.attr("height", function(){return 5;})
 					.attr("y", function(d,i){return y(i);})
 					.attr("fill", function(d){if ((d.key).indexOf("ranger") > -1) {
-												return "red";
+												return "#a6cee3";   //ranger azzurro 
 											} else if ((d.key).indexOf("general-gate") > -1) {
-												return "green";
+												return "#1f78b4"; // general gate blu
 											} else if ((d.key).indexOf("entrance") > -1){
-												return "blue"; 
+												return "#b2df8a";  //verdino entrance
 											} else if ((d.key).indexOf("camping") > -1){
-												return "gray";
-											} else { //gates
-												return "black";
+												return "#33a02c"; //verde camping
+											} else { 
+												return "#fb9a99"; //rosa gates 
 											}
 									})
 					.on("click", function(){d3.select("#svga").remove();
 											bar("a");
 											});
-
-	});
-
-	
-}
-
-function assignColor(){
-	c 
-	return "steelblue";
+	});	
 }
 
 function handleMouseIn(d)
@@ -268,7 +333,7 @@ function scatter(name){
 		//	console.log(max);
 			
 			//var margin = {top: 20, right: 20, bottom: 50, left: 50};
-			var h = document.getElementById(name).scrollHeight;
+			var h = document.getElementById(name).clientHeight;
 			var w = document.getElementById(name).offsetWidth;
 			    w = w - margin.left - margin.right;
 				h = h - margin.top - margin.bottom;
@@ -347,14 +412,14 @@ function time(name){
 				var time = parseTime(d.key);
 				d.key = format(time);
 				})
-			//console.log(ordered);
+			console.log(ordered);
 			
 			var ordered1 = d3.nest()
 						.key(function(d){return d['key'];})
 						.entries(ordered);
 			//console.log(ordered1);
 			
-			var h = document.getElementById(name).scrollHeight;
+			var h = document.getElementById(name).clientHeight;
 			var w = document.getElementById(name).offsetWidth;
 			    w = w - margin.left - margin.right;
 				h = h - margin.top - margin.bottom;
